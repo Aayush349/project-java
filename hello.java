1edfb680744ff9da1abda91a214b15d795718d01
aayush <aayushsahu867@gmail.com>
2024-07-29 00:27:27 +0530

    1commit

---
+++ hello.java
@@ -0,0 +1,9 @@
+ class main 
+ {
+    public static void main (string args[]){
+        System.out.println("hellio");
+         System.out.println("hiii");
+          System.out.println("hellio");
+    }
+ }
+ 
