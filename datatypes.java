9c08e2391093ccb13728f11fa97cfe405904e236
Aayush349 <aayushsahu867@gmail.com>
2024-08-05 23:01:39 +0530

    Data type program

---
+++ datatypes.java
@@ -0,0 +1,23 @@
+
+
+public class datatypes {
+    public static void main (String args[]){
+
+        int a=20;
+        char b='c';
+        double d =2.32;
+        long l =5654645;
+        boolean g=true;
+           System.out.println(a);
+           System.out.println(b);
+                      System.out.println(l);
+                      System.out.println(d);
+                      System.out.println(d);
+                      System.out.println(g);
+
+
+
+    }
+
+
+}
