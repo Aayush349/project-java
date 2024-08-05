98054ab7700e65454ebae69297bf46474741d8a0
Aayush349 <aayushsahu867@gmail.com>
2024-08-05 23:03:35 +0530

    diffrent pattern

---
+++ pattern.java
@@ -0,0 +1,48 @@
+public class pattern{
+    public static void main(String[] args) {
+        // simple triangle pattern
+        for(int i =1;i<=5;i++)
+        {
+
+         for (int j=1;j<=i;j++)
+         {
+           System.out.print("*");
+        }
+        System.err.println("");
+        }
+
+
+        System.err.println("");
+        System.err.println("");
+
+//patten 2
+  
+for (int i =0;i<5;i++)
+{
+    for (int j=5;j>i;j--)
+    System.out.print("*");
+        System.out.println();
+
+}
+
+
+
+
+
+
+//pattern 4
+  for(int i =1;i<=5;i++)
+        {char c='A';
+
+
+         for (int j=1;j<=i;j++)
+         {
+            
+           System.out.print(c );
+                       c+=1;
+
+        }
+        System.err.println("");
+        }
+
+    }}
